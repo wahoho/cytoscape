675440386dfd5da32a97d46d964bfbb1b02605e5
Jason Yap <wahoho.yap@gmail.com>
2024-08-22 23:36:22 +0900

    refactored

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
           <p>
-              <a className="underline" href="/cytoscape">Cytoscape</a>
+              <Link className="underline" href="/cytoscape">Cytoscape</Link>
           </p>
       </div>
     </main>
