24b37047c752f42e8ec2aae9515a5c787cb28ba5
Jason Yap <wahoho.yap@gmail.com>
2024-08-22 23:35:45 +0900

    refactored

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-          <h2>AI Report</h2>
-          <p>
-              <a className="underline" href="/d3js">D3JS</a>
-          </p>
           <p>
               <a className="underline" href="/cytoscape">Cytoscape</a>
           </p>
-
       </div>
     </main>
   );
